ba2b50fc763fef80a226e89b875fb7886356e035
Luis Zubia <soyzubia@gmail.com>
2022-12-12 11:52:08 -0600

    Add code for use the App module and provide the user interactions.

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,3 +4,18 @@
 
 // -- (I)mport module App - it provides the App class to create valid App object -- //
 import App from '../modules/app.js';
+
+// -- app - is the interface with the user -- //
+const app = new App();
+
+// - When the DOM is ready, the books from the localStorage are shown - //
+window.addEventListener('DOMContentLoaded', app.listBooks());
+
+// -- Listen to the submit event on the form -- //
+app.addForm.addEventListener('submit', (e) => app.addBook(e));
+
+// -- Listen to the click event on the list of books -- //
+app.booksUl.addEventListener('click', (e) => app.removeBook(e));
+
+// -- Listen to the click event on the navbar options -- //
+app.menu.addEventListener('click', (e) => app.showMenu(e));

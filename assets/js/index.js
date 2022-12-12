1ff224516d32daba04ed9c80ac12a2cc34e0e480
Luis Zubia <soyzubia@gmail.com>
2022-12-12 11:10:55 -0600

    Import Book from book.js module.

---
+++ assets/js/index.js
@@ -0,0 +1,6 @@
+// This is the JS source entry point for the project.
+// where we link the different modules required for
+// the project to work.
+
+// -- (I)mport module Book - it provides the Book class to create valid Book objects -- //
+import Book from '../modules/book.js';
